f9e64bc8cfd42437bc8cb14d41da1e92b82d57aa
Pvpexmc <55208721+Pvpexmc@users.noreply.github.com>
2019-10-04 14:15:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,27 +3,27 @@ const client = new Discord.Client();
 const prefix = "$";
 
 
-client.on('message', message => { // اقتراح
+client.on('message', message => {
     if (message.content.startsWith(prefix + 'sug')) {
         if (message.author.bot) return
         if (!message.guild) return message.reply('**:x: This Commands Just In Server**').then(v => {v.react('❌')})
         var args =  message.content.split(' ').slice(1).join(' ')
         if (!args) return message.reply('Type You Suggestion').then(c => {c.delete(5000)})
-        let Room = message.guild.channels.find(`name`, "🍂》suggestions")
+        let Room = message.guild.channels.find(`name`, "suggestions")
         if (!Room) return message.channel.send("Can't find suggestions channel.").then(d => d.react('❌'))
         let embed = new Discord.RichEmbed()
         .setColor('RANDOM')
         .setAuthor(`Vote on ${message.author.username}'s suggestion`, message.author.avatarURL)
-        .addField('**Suggestion**',`${args}`)
-        .setThumbnail(message.author.avatarURL)
-        .setFooter(`ID: ${message.author.id}`)
-        Room.sendEmbed(embed).then(c => {
-            c.react('✅').then(() => 
-                c.react('❌'))
-
-        }).catch(e => console.error(e)
-        )
-    }
+       .addField('**Suggestion**',`${args}`)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(`ID: ${message.author.id}`)
+       Room.sendEmbed(embed).then(c => {
+           c.react('✅').then(() =>
+               c.react('❌'))
+           
+       }).catch(e => console.error(e)
+       )
+   }
 });
 
 client.on("message", message => { // تقديم اداره
